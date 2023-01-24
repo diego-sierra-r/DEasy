6a0ac102e4d3e84265ff61a0f6f2baaddef82306
Diego <dsierrar@ut.edu.co>
2023-01-24 13:11:07 -0500

    Define preliminary UI

--- R/server.R
+++ R/server.R
@@ -1,17 +1,47 @@
 library(shiny)
+options(shiny.maxRequestSize = 100 *
+          1024^2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  raw_counts_data <- reactive({
+    req(input$raw_counts)
+    file <- vroom::vroom(input$raw_counts$datapath,delim = ",")
+  })
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  output$raw_counts <- DT::renderDataTable({
+    DT::datatable(
+      raw_counts_data(),
+      rownames = FALSE,
+      options = list(
+        columnDefs = list(list(targets = '_all', width = "2px")),
+        columns.type = "num",
+        scrollCollapse = TRUE,
+        searching = FALSE,
+        lengthChange = TRUE,
+        ordering = FALSE,
+        paging = TRUE,
+        pageLength = 20,
+        scrollX = "200px",
+        scrollY = "530px")
+    )
+  })
+    
+    output$DE_results<- renderDataTable({
+      mtcars
+    })
+    
+    output$plot1 <- renderPlot({
+      plot(1:3)
+    })
+    output$plot2 <- renderPlot({
+      plot(3:6)
+    })
+    output$plot3 <- renderPlot({
+      plot(6:9)
+    })
+    output$plot4 <- renderPlot({
+      plot(9:12)
     })
 
 })
--- R/ui.R
+++ R/ui.R
@@ -1,24 +1,53 @@
 library(shiny)
+library(RColorBrewer)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
+  
+  titlePanel(title = "DEasy", windowTitle = "DEasy!"),
+  sidebarLayout(
+    sidebarPanel(
+      titlePanel("Upload files"),
+      fileInput(inputId = "sampleinfo",label = "Sample imformation",),
+      fileInput(inputId = "raw_counts",label = "Raw counts"),
+      titlePanel("Parameters"),
+      selectInput(inputId = "pgk",
+                  label = "Differential expression package to use:",
+                  choices = c("DESeq2","edgeR")),
+      numericInput(inputId = "pvalue",
+                   label = "p-value",
+                   step = 0.01,
+                   value = 0.05,
+                   min = 0.01,
+                   max = 0.99),
+      numericInput(inputId = "Treshold",
+                   label = "p-value",
+                   step = 0.5,
+                   value = 2,
+                   min = 0.5,
+                   max = 4),
+      selectInput(inputId = "palette",
+                  label = "Color palette",
+                  choices = rownames(brewer.pal.info))
+    ),
+    mainPanel(
+      tabsetPanel(type = "pills",
+        tabPanel(title = "Input data",
+                           DT::DTOutput("raw_counts")),
+        tabPanel("Results",
+                dataTableOutput("DE_results")),
+        tabPanel("Plots",
+                column(6,
+                  plotOutput("plot1"),
+                  plotOutput("plot2")
+                  ),
+                column(6,
+                  plotOutput("plot3"),
+                  plotOutput("plot4")
+                  
+                ))
+      )
+    ),
+  )
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
 ))
