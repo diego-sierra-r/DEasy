67aadc034772e192e6fb4762f86195365cba5439
Diego <dsierrar@ut.edu.co>
2023-01-25 08:10:05 -0500

    update data examples

--- tests/print_debugging.R
+++ tests/print_debugging.R
@@ -1,7 +1,14 @@
 # Renam2 HTSEQ file
-ara <- read.csv("~/Documentos/R/my_pkgs/DEasy/HTSeqcount_EDITED_mapeo_clean_ALL_AraBAN.txt",sep = "\t")
-colnames(ara) <- colnames(ara) |> 
-  stringr::str_replace_all("HTSeqcount_mapeo_cleanAraBAN",replacement = "Sample_")
-write.csv(x = ara, "~/Documentos/R/my_pkgs/DEasy/ara_counts.csv",row.names = F)
-
-
+ara <-
+  read.csv(
+    "~/Escritorio/Anexos objetivo 04/R_Transcriptome/Resultados DEG/DEGs_preliminareszip/HTSeqcount_EDITED_mapeo_clean_ALL_AraBAN.txt",
+    sep = "\t"
+  )
+colnames(ara) <- colnames(ara) |>
+  stringr::str_replace_all("HTSeqcount_mapeo_cleanAraBAN", replacement = "Sample_") |> 
+  stringr::str_replace_all(".bam.txt",replacement = "")
+write.csv(
+  x = ara,
+  "~/Documentos/R/my_pkgs/DEasy/tests/ara_counts.csv",
+  row.names = F
+)
