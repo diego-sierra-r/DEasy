0d54eeb52f86a7cdcf510cff7021b6c704fba5a8
Diego <dsierrar@ut.edu.co>
2023-04-10 21:59:47 -0500

    minor changes

---
+++ render_README.R
@@ -0,0 +1,2 @@
+rmarkdown::render("~/Documentos/R/my_pkgs/DEasy/README.Rmd",output_format = "html_document")
+rmarkdown::render("~/Documentos/R/my_pkgs/DEasy/README.Rmd",output_format = "github_document")
