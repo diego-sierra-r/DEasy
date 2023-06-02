c969e958a00afb14845386befc15a61bf735734d
diego-sierra-r <dsierrar@ut.edu.co>
2023-06-02 17:14:00 -0500

    minor changes to README

--- render_README.R
+++ render_README.R
@@ -1,2 +1,5 @@
+
+# agregar a pathhandler (no esta funcionado en windows)
+
 rmarkdown::render("~/Documentos/R/my_pkgs/DEasy/README.Rmd",output_format = "html_document")
 rmarkdown::render("~/Documentos/R/my_pkgs/DEasy/README.Rmd",output_format = "github_document")
